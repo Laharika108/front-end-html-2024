7ca6ac41dddbb8f41dc08ddedfe8e3064d55f594
Laharika108 <laharikakarupalli@gmail.com>
2024-09-05 19:35:03 +0530

    togglebutton and darkmode

---
+++ js-demos1/way to include-js/ways-to-create-function/functionDemo.js
@@ -0,0 +1,14 @@
+//here we will define the function
+// all the functions here do not take any arguments and do not return any values
+//first way of defining a function
+function display(){
+    console.log("first way of defining a function");
+}
+//second way of defining a function
+let display2 = function(){
+    console.log("second way of defining a function");
+}
+//third way of defining a function
+let display3 = () => {
+    console.log("third way of defining a function")
+}
